0358a58dd1d5a72ab013b5f08eaf1d2802e0a14f
Tony H. <tony.hirschligau@googlemail.com>
2025-04-25 02:20:38 +0200

    update moveable-object
    added gravity function

--- classes/moveable-object.class.js
+++ classes/moveable-object.class.js
@@ -6,26 +6,38 @@ class MoveableObject {
   currentImage = 0;
   speed = 1.2;
   otherDirection = false;
+  speedY = 0;
+  acceleration = 2.5;
+  applyGravity() {
+    setInterval(() => {
+      if (this.y < 180) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
+      }
+    }, 1000 / 25);
+    
+  }
+
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
   }
 
-  loadImages(array){
-    array.forEach(path => {
+  loadImages(array) {
+    array.forEach((path) => {
       let img = new Image();
       img.src = path;
       this.imageCache[path] = img;
     });
   }
 
-  moveLeft(){
+  moveLeft() {
     setInterval(() => {
-        this.x -= this.speed;
-      }, 1000 / 60);
+      this.x -= this.speed;
+    }, 1000 / 60);
   }
 
-  playAnimation(image){
+  playAnimation(image) {
     let i = this.currentImage % image.length;
     let path = image[i];
     this.img = this.imageCache[path];
